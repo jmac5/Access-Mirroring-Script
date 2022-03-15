849bd3d37cd0410c1bac0b30f7dd4c628f2784a7
jmac5 <56899560+jmac5@users.noreply.github.com>
2022-03-15 08:52:11 -0400

    Update Access Mirroring (Public Version).ps1
    Updated ReadMe in script to match GitHub

--- Access Mirroring (Public Version).ps1
+++ Access Mirroring (Public Version).ps1
@@ -17,24 +17,30 @@ $Logfile = "<path>\Mirror.log" #log files for troubleshooting or auditing. You m
 
 <#--------------------------------------------README-------------------------------------------------------
 
+The base functionality of this script, as written, is such:
+- Check for users with 2 active badges in Lenel DB (MS SQL + PowerShell sqlcmd)
+- Check access on those badges (MS SQL + PowerShell sqlcmd)
+- If one badge has more access than another, get the access level ID, activate and deactivate dates, and apply it to the badge without the access using OpenAccess API.
+
 Things you MUST change in this script for your org:
-
 * DB Server name ($dbserver)
-* OpenAccess server, or your primary OnGuard server (in the RestMethod strings, or $appServer if you want)
-* AppIDs within the headers for your environment/apps
-
+* OpenAccess server, or your primary OnGuard server ($AppServer:<portForAPI>). Port is 8080 by default in the script. 
+* AppIDs within the headers for your environment/apps ($AppID)
+  
 You will also need the "sqlcmd" module installed for Powershell, which works with MS SQL DB Instances. 
-If you run Oracle, this script won't work (it may be serviceable to convert, but it is untested!)
+If you run an Oracle DB, this script won't work (it may be serviceable to convert, but it is untested!)
 
 When you get it working for your environment, add it to a scheduled task on a server, set to run as a service
 account, so it will check periodically for problems. Having it check regularly will keep your environment in
 good shape. The first run may take a long time, if there are lots of problems. That's OK. 
 
-Other than that, have fun! Always kick the tires on a test server or DB first!! This script was developed with
+Other than that, have fun! **Always kick the tires on a test server or DB first!!** This script was developed with
 my org's needs in mind, and yours may be different. I tried to comment on a lot of the functions, but some may 
 still be confusing. I recommend debugging and stepping through while watching variables to figure out what 
 may be going on, if you're lost. 
-
+  
+In the process of sanitizing this for public consumption, some things have been ripped out and replaced with generic placeholders. I highly suggest checking all the lines (there really aren't that many) for any leftover empty references before running.
+  
 Jim
 
 #>
